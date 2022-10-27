c384ed2b8a06bfec66def9be9eaf08cdcbe73010
fabiooo4 <fabioirimie@gmail.com>
2022-10-27 10:52:05 +0200

    first commit

---
+++ quadraticEquation.php
@@ -0,0 +1,16 @@
+<html>
+<link rel="stylesheet" href="style.css">
+<div class="main">
+  <head> 
+    <title>PHP Test</title>
+  </head>
+    
+      <body class="mainBg">
+        <h1>Equazione di secondo grado</h1>
+            <?php
+               
+
+            ?>
+        </body>
+</div>
+</html>
